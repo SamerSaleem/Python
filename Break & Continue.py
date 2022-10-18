69ab2b40e7abe0f24547a2233645793de962863a
Samer Rafid <samer.rafid@gmail.com>
2022-10-18 13:46:59 +0300

    adding comments to make code clear.

--- Break & Continue.py
+++ Break & Continue.py
@@ -1,6 +1,10 @@
+#The comparison constant is configured below:
 largest_number = -99999999
+
 counter = 0
 
+#the while true means keep doing the loop without any condition until user enters the -1 which will break the loop.
+#if you dont use if/break sentence here, then you will need to add the condition with same line as "while".
 while True:
     number = int(input("Enter a number or type -1 to end program: "))
     if number == -1:
