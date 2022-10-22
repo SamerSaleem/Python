18572f509393c9f5889c9ce62e838e44eb2e107c
Samer Rafid <samer.rafid@gmail.com>
2022-10-22 08:50:04 +0300

    updating the comment section.

---
+++ #Continue statement challenge 2.py
@@ -0,0 +1,9 @@
+#Continue statement challenge 2
+vowel_word = str(input("Please enter a word!: \n"))
+letter={"a": "", "e": "", "o": "", "i": "", "u": ""}
+for letter in vowel_word:
+    letter = vowel_word.replace()
+    print(letter)
+
+#this program is not complete,,,,,need to resolve it later,
+#you should make the user enters a word and the program should remove the vowels and print the word in upper case letters.
