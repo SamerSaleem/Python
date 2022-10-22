0f4974ffa5ccf2a6f6e526657c2d523dd54a1e5c
Samer Rafid <samer.rafid@gmail.com>
2022-10-22 09:29:06 +0300

    while-else feature simple program

---
+++ While-Else feature.py
@@ -0,0 +1,8 @@
+#While/Else in a program....very nice feature....note: the else will only run one time....always like this....
+
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
