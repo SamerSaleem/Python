5d48046a4c8550b3b8d61b5be166990282f52314
Samer Rafid <samer.rafid@gmail.com>
2022-10-23 10:57:32 +0300

    adding comment at the end explaining the range...

--- #simple while else program.py
+++ #simple while else program.py
@@ -13,3 +13,7 @@ for num in i:
     print(num - 1)
 else:
     print(num)
+
+#the range below will print x starting from 0 and jumping by 3 every time but should be less than 10.
+for x in range(0, 10, 3):
+    print("x is equal", x)
