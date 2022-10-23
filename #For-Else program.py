5dcbc5b48b77b1f36c3900d38b45d6e5e21c4297
Samer Rafid <samer.rafid@gmail.com>
2022-10-23 08:40:05 +0300

    adding another comment before else.

--- #For-Else program.py
+++ #For-Else program.py
@@ -4,6 +4,7 @@ i = 111
 #below range is not going to work, because 1 is less than 2, but if you change the value to 11 then it will show different output.
 for i in range(2, 1):
     print(i)
+#the else here will print the original value of i if the for loop couldn't print it as range.so this is the condition.
 else:
     print("else:", i)
 
