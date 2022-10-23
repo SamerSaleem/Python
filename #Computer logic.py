7cc2e986560bda7e565b98055c8b46c3690e7a3d
Samer Rafid <samer.rafid@gmail.com>
2022-10-23 15:02:52 +0300

    creating simple code with logical operators

---
+++ #Computer logic.py
@@ -0,0 +1,15 @@
+#Computer logic
+#we have the (and, or) operators, in "and" only the truth vs. truth is true, and in OR operator only the false vs false
+#is considered false.
+#also there is (not) operator which turns the true to false and vice versa.
+
+
+a = 9
+b = 10
+if a or b == 10:
+    print("one of them is equal to 10")
+else:
+    print("A or B is not equal to 10")
+
+
+
