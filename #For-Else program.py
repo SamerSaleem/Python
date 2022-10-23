553922929904c1267523555e72cef441a7083dce
Samer Rafid <samer.rafid@gmail.com>
2022-10-23 08:38:11 +0300

    first version of the simple code.

---
+++ #For-Else program.py
@@ -0,0 +1,9 @@
+#For-Else program
+i = 111
+
+#below range is not going to work, because 1 is less than 2, but if you change the value to 11 then it will show different output.
+for i in range(2, 1):
+    print(i)
+else:
+    print("else:", i)
+
