ee878357172e8431983ec48345d44f782ab11cc3
Samer Rafid <samer.rafid@gmail.com>
2022-10-25 08:55:32 +0300

    first upload

---
+++ #using sys module.py
@@ -0,0 +1,10 @@
+#using sys module
+import sys
+from urllib import response
+while True:
+    print('Type Exit to Exit!')
+    response= input()
+    if response == 'exit':
+        sys.exit()
+    print('you typed' + response + '.')
+    
