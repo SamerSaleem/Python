e9e0472ffac23287e2ad92b34c6746f33e527f23
Samer Rafid <samer.rafid@gmail.com>
2022-10-25 08:52:23 +0300

    randomizing numbers code.

---
+++ #randomizing-numbers.py
@@ -0,0 +1,6 @@
+#randomizing-numbers
+import random
+
+for x in range(100):
+    print(random.randint(1, 99))
+#This simple code imports random module which can be used to randomize a range of numbers.
