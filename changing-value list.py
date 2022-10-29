8463ddc16ba255b554a3e8e693ef589faedf61d4
Samer Rafid <samer.rafid@gmail.com>
2022-10-29 08:35:52 +0300

    updating a value inside a list

---
+++ changing-value list.py
@@ -0,0 +1,10 @@
+#lists-collecting data
+#how to use lists and print positions inside the list.
+numbers = [1, 3, 10, 11, 15]
+
+print("Position 4 of the numbers list is: ", numbers[3]) #this will print position 4 
+print("While the whole numbers list is : ", numbers) 
+
+#change value inside a list
+numbers[4] = 111
+print('This is the updated list of numbers:', numbers)
