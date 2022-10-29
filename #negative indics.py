084ed279fa73a0b28ee8589e9d5ccae1c55c6f42
Samer Rafid <samer.rafid@gmail.com>
2022-10-29 14:12:43 +0300

    negative indics upload.

---
+++ #negative indics.py
@@ -0,0 +1,11 @@
+#negative indics
+import numbers
+
+
+numbers = [1, 100, 109, 113, 67, 98]
+
+print(numbers)
+
+print('print the first number from the end or the last number', numbers[-1]) #prints the first number from the end
+
+print('print the 4th number from the end', numbers[-4])  #prints the forth number before the last
