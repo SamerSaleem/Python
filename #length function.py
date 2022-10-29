971c7e0e70a0fddad0f7b04a17626e8aa9e565e3
Samer Rafid <samer.rafid@gmail.com>
2022-10-29 14:08:07 +0300

    added delete function at the end.

--- #length function.py
+++ #length function.py
@@ -9,3 +9,7 @@ numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
 print("Previous list content:", numbers)  # Printing previous list content.
 
 print("\nList length:", len(numbers))  # Printing the list's length.
+
+del numbers [0]  #deleting a number from the list.
+print('Removing one of the numbers of this list is in progress...\n') #comment for explanation. 
+print ('the new length is', len(numbers))
