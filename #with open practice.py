3697f2cce0dce8c0c081809751f182fc882ce155
Samer Rafid <samer.rafid@gmail.com>
2022-10-30 08:47:08 +0300

    nothing.

--- #with open practice.py
+++ #with open practice.py
@@ -4,4 +4,3 @@ with open ('parsing.csv') as excel: #open and then read it with read
 
 with open ('parsing.csv', 'a+') as excel:   #this will append to the end of the file
     excel.write(input('Please write the name of your node: \n'))
-    
