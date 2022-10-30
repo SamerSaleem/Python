587fd9891ea6eaa1ec831d16ed88e45adf101192
Samer Rafid <samer.rafid@gmail.com>
2022-10-30 08:40:05 +0300

    added user input prompt at the end.

--- #with open practice.py
+++ #with open practice.py
@@ -3,5 +3,5 @@ with open ('parsing.csv') as excel: #open and then read it with read
     print(excel.read())
 
 with open ('parsing.csv', 'a+') as excel:   #this will append to the end of the file
-    excel.write('Wireless\n')
+    excel.write(input('Please write the name of your node: \n'))
     
