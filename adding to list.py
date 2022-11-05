8f065148ba65b64474a946c76a7110d4d1b986e1
Samer Rafid <samer.rafid@gmail.com>
2022-11-05 12:12:56 +0300

    adding to list.

---
+++ adding to list.py
@@ -0,0 +1,7 @@
+#adding to list using for loop.
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.append(i + 1)   #this will make the code add to list from the range of the loop
+
+print(my_list)
