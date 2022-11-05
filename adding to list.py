cd90c46dfe2e2a488971613f74b48e20b2f297da
Samer Rafid <samer.rafid@gmail.com>
2022-11-05 12:31:37 +0300

    updating.

--- adding to list.py
+++ adding to list.py
@@ -1,7 +1,14 @@
 #adding to list using for loop.
 my_list = []  # Creating an empty list.
 
-for i in range(50):
+for i in range(5):
     my_list.append(i + 1)   #this will make the code add to list from the range of the loop
 
 print(my_list)
+
+my_list = [1, 2, 3, 4]
+total = 0
+
+for x in my_list:
+    total += x
+print('your total is', total)
