fed7deed09053456a8439050d60b7d96b0a19c8c
Samer Rafid <samer.rafid@gmail.com>
2022-11-06 14:47:29 +0300

    immutables tuples

---
+++ #tuples-learn.py
@@ -0,0 +1,9 @@
+#tuples-learn
+#tuples are immutables which is better when you need to use constant data through the program.
+
+samer = ('Kani', 'Ahmed', 'Arya')
+print ('your data type is', type(samer))
+
+print (samer[0])
+
+samer[0]= '15'  #this line will not make change to the tuples (immutable)
