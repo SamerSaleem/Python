d18f3e8f418ee459be91ec90aead953892de1d21
Samer Rafid <samer.rafid@gmail.com>
2022-11-12 14:00:03 +0300

    sorting letters and numbers

---
+++ sorting-lists.py
@@ -0,0 +1,7 @@
+my_list = [8, 7, 9, 10, 20, 2]
+
+my_list.sort()  #this method will change the variable my_list so you can print again and it will be sorted.
+print(my_list)
+abc= ['a', 'f', 'g', 'e', 'b']
+abc.sort()
+print(abc)
