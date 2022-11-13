31a0079080e46016c316ce09a224d3f6221a5306
Samer Rafid <samer.rafid@gmail.com>
2022-11-13 09:22:53 +0300

    upload

---
+++ learnloads_json.py
@@ -0,0 +1,12 @@
+import json
+
+with open ("json_sample.json") as samer:
+    json_data = samer.read()
+
+json_dict= json.loads(json_data)
+print ("the data type is: ", (type(json_dict)))
+print(json_dict)
+
+#lets modify the data
+json_dict ['interface'] ['description'] = 'backuplink'
+print(json_dict)
