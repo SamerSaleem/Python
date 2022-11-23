703723995e5cd89a53b22454935f6cb379285292
Samer Rafid <samer.rafid@gmail.com>
2022-11-23 08:32:15 +0300

    adding comments

--- list-123.py
+++ list-123.py
@@ -3,8 +3,8 @@ to_find = 55  #this number will be checked if existing in the list above or not.
 found = False
 
 for i in range(len(my_list)):
-    found = my_list[i] == to_find
-    if found:
+    found = my_list[i] == to_find #found will be matching any number in the list compared with to_find.
+    if found:  #this means if found became True then break the loop.
         break
 
 if found:
