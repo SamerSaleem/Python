ac3c91e8fc38732068f9af21d7f1936cae2d2d8e
Samer Rafid <samer.rafid@gmail.com>
2022-11-23 08:23:05 +0300

    adding comment

--- list-123.py
+++ list-123.py
@@ -1,5 +1,5 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-to_find = 5
+to_find = 55  #this number will be checked if existing in the list above or not.
 found = False
 
 for i in range(len(my_list)):
@@ -8,6 +8,6 @@ for i in range(len(my_list)):
         break
 
 if found:
-    print("\nElement found at index", i)
+    print("\n Element found at index", i)
 else:
-    print("absent")
+    print("\n absent")
